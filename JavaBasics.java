99cedecf0cca87df32470b49d8f485de49193c86
anjaliniranjan027 <142167388+anjaliniranjan027@users.noreply.github.com>
2023-08-14 18:14:05 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,13 @@
+public class JavaBasics {
+    public static void main(String []args) {
+        int a = 10 ;
+        int b = 5;
+        int sum = a + b ;
+        String name = "Tony Stark" ;
+        System.out.println(b) ;
+        
+        
+
+
+    }
+}
